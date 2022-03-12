f5ae5b1e2d48e322e364eb6be6682c42b487ea6a
Yrua Riley <yyriley0124@gmail.com>
2022-03-12 00:35:19 -0600

    Codepath - Twitter Client Part 1

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -0,0 +1,55 @@
+package com.codepath.apps.restclienttemplate
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.codepath.apps.restclienttemplate.models.Tweet
+
+class TweetsAdapter (val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        val view = inflater.inflate(R.layout.item_tweet, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    //populates data through view holder
+    override fun onBindViewHolder(holder: TweetsAdapter.ViewHolder, position: Int) {
+        val tweet: Tweet = tweets.get(position)
+
+        holder.tvUserName.text = tweet.user?.name
+        holder.tvTweetBody.text = tweet.body
+
+        Glide.with(holder.itemView).load(tweet.user?.publicImageURL).into(holder.ivProfileImage)
+    }
+
+    override fun getItemCount(): Int {
+        return tweets.size
+    }
+
+    // Clean all elements of the recycler
+    fun clear() {
+        tweets.clear()
+        notifyDataSetChanged()
+    }
+
+    // Add a list of items -- change to type used
+    fun addAll(tweetList: List<Tweet>) {
+        tweets.addAll(tweetList)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
+        val tvUserName = itemView.findViewById<TextView>(R.id.tvUsername)
+        val tvTweetBody = itemView.findViewById<TextView>(R.id.tvTweetBody)
+
+    }
+
+}
